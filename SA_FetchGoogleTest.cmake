23f267fd8d429497ed315eec37e6a0a5250c0bc8
mrouffet <maximerouffet@gmail.com>
2022-04-18 12:11:27 +0900

    [Split] FetchModule / FetchGoogleTest

---
+++ SA_FetchGoogleTest.cmake
@@ -0,0 +1,30 @@
+# Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+
+include(FetchContent)
+
+
+# Clone GoogleTest repository at config time and add it to ThirdParty/.
+# Automatically init git submodules.
+#
+# USAGE:
+#	SA_FetchGoogleTest()
+#
+function(SA_FetchGoogleTest)
+
+	if(NOT TARGET gtest)
+
+		message("Fetching Google Test...")
+
+		FetchContent_Declare(
+			googletest
+			GIT_REPOSITORY https://github.com/google/googletest.git
+			GIT_TAG        release-1.11.0
+			SOURCE_DIR		${CMAKE_SOURCE_DIR}/ThirdParty/googletest
+		)
+
+		FetchContent_MakeAvailable(googletest)
+
+	endif()
+
+endfunction()
--- SA_FetchModule.cmake
+++ SA_FetchModule.cmake
@@ -39,30 +39,3 @@ function(SA_FetchModule MODULE_NAME)
 	endif()
 
 endfunction()
-
-
-
-# Clone GoogleTest repository at config time and add it to ThirdParty/.
-# Automatically init git submodules.
-#
-# USAGE:
-#	SA_FetchGoogleTest()
-#
-function(SA_FetchGoogleTest)
-
-	if(NOT TARGET gtest)
-
-		message("Fetching Google Test...")
-
-		FetchContent_Declare(
-			googletest
-			GIT_REPOSITORY https://github.com/google/googletest.git
-			GIT_TAG        release-1.11.0
-			SOURCE_DIR		${CMAKE_SOURCE_DIR}/ThirdParty/googletest
-		)
-
-		FetchContent_MakeAvailable(googletest)
-
-	endif()
-
-endfunction()
