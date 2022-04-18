f8d369a601abb3a5e64b4172351f445c1e3e2503
mrouffet <maximerouffet@gmail.com>
2022-04-18 11:21:20 +0900

    [Add] fetch google test helper function

--- SA_FetchModule.cmake
+++ SA_FetchModule.cmake
@@ -27,3 +27,23 @@ function(SA_FetchModule _MODULE_NAME)
 	endif()
 
 endfunction()
+
+
+function(SA_FetchGoogleTest)
+
+	if(NOT TARGET gtest)
+
+		message("Fetching Google Test...")
+
+		FetchContent_Declare(
+			googletest
+			GIT_REPOSITORY https://github.com/google/googletest.git
+			GIT_TAG        release-1.11.0
+			SOURCE_DIR		${CMAKE_SOURCE_DIR}/ThirdParty/googletest
+		)
+
+		FetchContent_MakeAvailable(googletest)
+
+	endif()
+
+endfunction()
