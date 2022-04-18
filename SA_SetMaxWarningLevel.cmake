8d56f9dcdca3f23646cb76c287c3d4b200b897c2
mrouffet <maximerouffet@gmail.com>
2022-04-18 13:09:13 +0900

    [Add] SetMaxWarningLevel link argument

--- SA_SetMaxWarningLevel.cmake
+++ SA_SetMaxWarningLevel.cmake
@@ -1,33 +1,63 @@
 # Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
 
-# Set mmaximum compilation Warning level for target.
+
+
+# Set maximum compilation Warning level for target.
 #
 # USAGE:
 #	SA_SetMaxWarningLevel(
-#		<target_name>
+#		TARGET		<target>
+#		LINK		<link_opt>
 #	)
 #
 # ARGUMENTS:
-#	TARGET_NAME
+#	TARGET
 #		Name of the target to set max warning level.
 #
-function(SA_SetMaxWarningLevel TARGET_NAME)
+#	LINK
+#		Target link option (PUBLIC | PRIVATE | INTERFACE). Default is PUBLIC.
+#
+function(SA_SetMaxWarningLevel)
+
+#{ Args
+
+	cmake_parse_arguments(
+		PARGS
+		""
+		"TARGET;LINK"
+		""
+		${ARGN}
+	)
+
+
+	if(NOT PARGS_TARGET)
+		message(FATAL_ERROR "Missing TARGET argument")
+	endif()
+
+
+	# Default PUBLIC.
+	if(NOT PARGS_LINK)
+		set(PARGS_LINK PUBLIC)
+	endif()
+
+#}
+
 
 	if(MSVC)
 
 		if(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang") # ClangCl
 
-			target_compile_options(${TARGET_NAME} PUBLIC -W4)
+			target_compile_options(${PARGS_TARGET} ${PARGS_LINK} -W4)
 
 		else() # MSVC
 
-			target_compile_options(${TARGET_NAME} PUBLIC /W4)
+			target_compile_options(${PARGS_TARGET} ${PARGS_LINK} /W4)
 
 		endif()
 
 	else() # GNU / Clang
 
-		target_compile_options(${TARGET_NAME} PUBLIC -Wall -Wextra)
+		target_compile_options(${PARGS_TARGET} ${PARGS_LINK} -Wall -Wextra)
 
 	endif()
 
