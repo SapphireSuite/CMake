778c673f4ffdeba83ffa6b6af2a8acc6138258e3
mrouffet <maximerouffet@gmail.com>
2022-09-13 18:10:54 +0900

    [Add] Target Interface Source

--- SA_TargetSources.cmake
+++ SA_TargetSources.cmake
@@ -112,6 +112,61 @@ endfunction(SA_TargetPrivateSources)
 
 
 
+# Add *interface* sources and include directory to a target (using target_sources and target_include_directories).
+#
+# USAGE:
+#	SA_TargetInterfaceSources(
+#		TARGET		<target>
+#		FOLDER		<interface_folder>
+#	)
+#
+# ARGUMENTS:
+#	TARGET
+#		Name of the cmake target.
+#
+#	FOLDER
+#		Interface source folder.
+#		Default is "Include".
+function(SA_TargetInterfaceSources)
+	
+  #{ Args
+
+	cmake_parse_arguments(
+		PARGS
+		""
+		"TARGET;FOLDER"
+		""
+		${ARGN}
+	)
+
+
+	if(NOT PARGS_TARGET)
+		message(FATAL_ERROR "Missing TARGET argument")
+	endif()
+
+
+	# Default "Include".
+	if(NOT PARGS_FOLDER)
+		set(PARGS_FOLDER Include)
+	endif()
+
+  #}
+
+	# Remove SA_ from target name to get folder name.
+	string(REPLACE "SA_" "" MODULE_NAME ${PARGS_TARGET})
+
+	file(GLOB_RECURSE SA_SOURCES_INTERFACE "${PARGS_FOLDER}/*.hpp")
+	message(VERBOSE "[SA] ${PARGS_TARGET} interface sources: ${SA_SOURCES_INTERFACE}")
+
+	source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/${PARGS_FOLDER}/SA/${MODULE_NAME} PREFIX Public FILES ${SA_SOURCES_INTERFACE})
+	target_sources(${PARGS_TARGET} INTERFACE ${SA_SOURCES_INTERFACE})
+
+	# Interface include directory (access from project's outside).
+	target_include_directories(${PARGS_TARGET} INTERFACE ${PARGS_FOLDER})
+
+endfunction(SA_TargetInterfaceSources)
+
+
 # Add sources and include directories to a target (using target_sources and target_include_directories).
 #
 # USAGE:
